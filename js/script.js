adc26211db515a6372b6b6873635d65c22509bc6
lee jiho <89637792+dlwlgh324@users.noreply.github.com>
2021-11-27 06:17:33 +0900

    포트폴리오 7차 업로드
    포트폴리오 7차 업로드

--- js/script.js
+++ js/script.js
@@ -11,6 +11,14 @@ $(document).ready(function () {
         });
     });
 
+    $(window).scroll(function () {
+        if ($(document).scrollTop() > 150) {
+            $('.gotop').addClass('gotop-show');
+        } else {
+            $('.gotop').removeClass('gotop-show');
+        }
+    });
+
     // 부드럽게 위치 이동하는 코드
     var moveEl = $('.move');
     // 클릭시 해당위치로 부드럽게 이동한다.
